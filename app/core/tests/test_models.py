6700d9505a780494028a9d8dd7f5fc9581389aff
bob <rkoertge@salesforce.com>
2021-07-06 23:24:39 -0400

    Added Validation to email

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -14,4 +14,17 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
-        
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        email = 'test@ExAMPLE.com'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password='test123'
+        )
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email rasies an error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None,'test123')
