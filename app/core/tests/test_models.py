515ca033329546b0001878eeb7151c9c8c9ebc05
bob <rkoertge@salesforce.com>
2021-07-06 23:36:18 -0400

    Cleanup and Added SuperUser Functionality

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
@@ -27,4 +28,14 @@ class ModelTests(TestCase):
     def test_new_user_invalid_email(self):
         """Test creating user with no email rasies an error"""
         with self.assertRaises(ValueError):
-            get_user_model().objects.create_user(None,'test123')
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
